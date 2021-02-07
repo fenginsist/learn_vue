788e2c5c9bab61b88f492848bbae0c4e4e16d29d
fengfl <fengfl@dataadt.com>
2021-02-07 18:31:31 +0800

    20210207:冯凡利：learn vue

---
+++ day05_webpack使用和配置/01-webpack的使用/02-webpack的配置/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'), // __dirname: 当前文件的目录
+    filename: 'bundle.js'
+  },
+}
