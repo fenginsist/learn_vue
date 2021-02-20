6bde07e3de7456af094d74be0ad17ef3e2413832
fengfanli <897961047@qq.com>
2021-02-20 23:14:21 +0800

    20210220:冯安辰：webpack

--- day05_webpack使用和配置/01-webpack的使用/02-webpack的配置/webpack.config.js
+++ day05_webpack使用和配置/01-webpack的使用/02-webpack的配置/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/main1.js',
+  entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'), // __dirname: 当前文件的目录
     filename: 'bundle.js'
