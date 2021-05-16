1d68b3e6b8f8abadbddb576142371f65ec47637c
fengfl <fengfl@dataadt.com>
2021-05-16 14:56:56 +0800

    20210516-1456:冯凡利：learn vue

---
+++ day09_axios/01_learn_axios/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h2>hello axios</h2>
+    <h2>{{item.message}}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        item: {
+          message: 'im message'
+        }
+      }
+    },
+    created() {
+      this.test()
+    },
+    computed: {
+      message: function () {
+        this.item.message = this.item.message + "123444"
+        // return this.item.message + "123444"
+      }
+    },
+    methods: {
+      test(){
+        console.log(this.item.message);
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ day09_axios/01_learn_axios/src/main.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import axios from "axios";
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
+
+axios({
+  url: 'http://123.207.32.32:8000/home/multidata',
+  method: 'get'
+}).then((res)=>{
+  console.log(res);
+})
+
+axios({
+  url: 'http://123.207.32.32:8000/home/data',
+  method: 'get',
+  // 专门针对 get 请求的 参数拼接
+  param: {
+    type: 'pop',
+    page: 1
+  }
+}).then(res=>{
+  console.log(res);
+})
